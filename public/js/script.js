c6e3492e32060d3567b714040f9c04e0ce502bfb
Marijo <marijo.torre.03@gmail.com>
2022-09-09 13:33:44 -0500

    upload project

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("adoptapet JS imported successfully!");
+  },
+  false
+);
---
+++ utils/consts.js
@@ -0,0 +1,3 @@
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/adoptapet";
+
+module.exports = MONGO_URI;
