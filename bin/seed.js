8ad7dc9990261de9d1513908272e7a4b092e6a76
Rodrigo Gutierrez Pacheco <rodrigogutierrezpacheco@gmail.com>
2022-09-13 20:59:01 -0500

    modified seed, views-signup-myPet-wall

--- bin/seed.js
+++ bin/seed.js
@@ -16,7 +16,8 @@ const pets = [
 	{petname:"Frijol", pet:"cat", dob:"07/06/2021",size:"small", personality:"playful", sociability:"sociable", city:"Cuernavaca", phone:"777123456", status:"looking for a family", photo:"https://images.pexels.com/photos/1741205/pexels-photo-1741205.jpeg?auto=compress&cs=tinysrgb&w=600"},
 	{petname:"Moscow", pet:"dog", dob:"09/04/2021",size:"big", personality:"playful", sociability:"sociable", city:"Cuernavaca", phone:"777123456", status:"looking for a family", photo:"https://www.nationalgeographic.com.es/medio/2021/03/09/perro_4da5a8be_800x1200.jpg"},
 	{petname:"Porfirio", pet:"cat", dob:"12/08/2021",size:"small", personality:"energetic", sociability:"sociable", city:"Cuernavaca", phone:"777123456", status:"looking for a family", photo:"https://images.pexels.com/photos/1314550/pexels-photo-1314550.jpeg?auto=compress&cs=tinysrgb&w=600"},
-	{petname:"Saly", pet:"dog", dob:"01/01/2021",size:"medium", personality:"relaxed", sociability:"sociable", city:"Cuernavaca", phone:"777123456", status:"looking for a family", photo:"https://st.depositphotos.com/1465111/1995/i/450/depositphotos_19959663-stock-photo-lets-go-for-a-walk.jpg"},
+	{petname:"Sally", pet:"dog", dob:"01/01/2021",size:"medium", personality:"relaxed", sociability:"sociable", city:"Cuernavaca", phone:"777123456", status:"looking for a family", photo:"https://st.depositphotos.com/1465111/1995/i/450/depositphotos_19959663-stock-photo-lets-go-for-a-walk.jpg"},
+	{petname:"Doroteo", pet:"cat", dob:"01/01/2021",size:"small", personality:"relaxed", sociability:"sociable", city:"Cuernavaca", phone:"777123456", status:"looking for a family", photo:"https://st3.depositphotos.com/15076092/18942/i/450/depositphotos_189426068-stock-photo-maine-coon-cat-looking-licking.jpg"},
 	
 
 ];
