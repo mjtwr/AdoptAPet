e5400b470c16540ab1d56ac76201a63cbd11cb1f
Rodrigo Gutierrez Pacheco <rodrigogutierrezpacheco@gmail.com>
2022-09-14 10:35:17 -0500

    modified seed & views

--- bin/seed.js
+++ bin/seed.js
@@ -8,6 +8,7 @@ const mongoose = require("mongoose");
 const MONGO_URI = require("../utils/consts");
 
 const Pet = require("../models/Pet.model");
+const Post = require("../models/Post.model");
 
 const pets = [
 	{ petname: "Hoshi", pet: "dog", dob: "06/09/2021", size: "big", personality: "energetic", sociability: "sociable", city: "Merida", name: "Marijo", phone: "973973", status:"adopted", photo:"https://st.depositphotos.com/1146092/1285/i/450/depositphotos_12854128-stock-photo-hungry-dog-food-bowl.jpg"},
@@ -18,9 +19,12 @@ const pets = [
 	{petname:"Porfirio", pet:"cat", dob:"12/08/2021",size:"small", personality:"energetic", sociability:"sociable", city:"Cuernavaca", phone:"777123456", status:"looking for a family", photo:"https://images.pexels.com/photos/1314550/pexels-photo-1314550.jpeg?auto=compress&cs=tinysrgb&w=600"},
 	{petname:"Sally", pet:"dog", dob:"01/01/2021",size:"medium", personality:"relaxed", sociability:"sociable", city:"Cuernavaca", phone:"777123456", status:"looking for a family", photo:"https://st.depositphotos.com/1465111/1995/i/450/depositphotos_19959663-stock-photo-lets-go-for-a-walk.jpg"},
 	{petname:"Doroteo", pet:"cat", dob:"01/01/2021",size:"small", personality:"relaxed", sociability:"sociable", city:"Cuernavaca", phone:"777123456", status:"looking for a family", photo:"https://st3.depositphotos.com/15076092/18942/i/450/depositphotos_189426068-stock-photo-maine-coon-cat-looking-licking.jpg"},
-	
+	];
 
-];
+	const postCommunity = [
+		{petname: "Arandano", name:"Alan", Comment: "I adopted Arandano 1 year ago, we´re happy now and we like to go adventure", adoptionDate:"02/06/2021"}
+	]
+
 mongoose
   .connect(MONGO_URI)
   .then((x) => {
@@ -32,3 +36,16 @@ mongoose
   .catch((err) => {
     console.error("Error connecting to mongo: ", err);
   });
+
+	mongoose
+  .connect(MONGO_URI)
+  .then((x) => {
+		return Post.create(postCommunity)
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
+
